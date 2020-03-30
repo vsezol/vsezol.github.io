83d0db7a8108a1910e8b16d1887793e700d7565f
vsezol <50067638+vsezol@users.noreply.github.com>
2020-03-30 21:38:45 +0700

    add quizer

---
+++ quizer/precache-manifest.2043588037a33318eb92dff01ad9bd51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3b1742b2102ace8a4b2e8ab3e8a34e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19469521c4c210ac36aa",
+    "url": "/static/css/main.b3c58021.chunk.css"
+  },
+  {
+    "revision": "25d2011f0766e8211106",
+    "url": "/static/js/2.13813626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13813626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19469521c4c210ac36aa",
+    "url": "/static/js/main.bc907388.chunk.js"
+  },
+  {
+    "revision": "0a6d902538761987f488",
+    "url": "/static/js/runtime-main.a2ebebe9.js"
+  }
+]);
