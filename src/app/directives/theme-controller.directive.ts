9bb803b498bd3f6386dbf625fff7fe85af37086e
vsezol <vsezold@gmail.com>
2023-02-27 22:11:49 +0300

    feature: make a preview and add toolbar theme

--- src/app/directives/theme-controller.directive.ts
+++ src/app/directives/theme-controller.directive.ts
@@ -1,7 +1,11 @@
-import { Directive, Inject, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, Inject, OnDestroy, Renderer2 } from '@angular/core';
 import { WINDOW } from '@app/declarations/constants/tokens/window.token';
 import { Theme, ThemeService } from '@bimeister/pupakit.common';
 import { fromEvent, Subscription } from 'rxjs';
+
+const COLOR_NEUTRAL_1000: string = '#1c1d22';
+const COLOR_NEUTRAL_1: string = '#ffffff';
 
 @Directive({
   selector: '[appThemeSync]',
@@ -12,7 +16,9 @@ export class ThemeSyncDirective implements OnDestroy {
 
   constructor(
     private readonly themeService: ThemeService,
-    @Inject(WINDOW) private readonly window: Window
+    @Inject(WINDOW) private readonly window: Window,
+    @Inject(DOCUMENT) private readonly document: Document,
+    private readonly renderer: Renderer2
   ) {
     this.applyPreferredTheme();
     this.subscription.add(this.watchColorSchemeChange());
@@ -32,10 +38,12 @@ export class ThemeSyncDirective implements OnDestroy {
   }
 
   private applyPreferredTheme(): void {
-    this.themeService.setTheme(this.getPreferredColorScheme());
+    const preferredTheme: Theme = this.getPreferredTheme();
+    this.themeService.setTheme(preferredTheme);
+    this.setBrowserToolbarTheme(preferredTheme);
   }
 
-  private getPreferredColorScheme(): Theme {
+  private getPreferredTheme(): Theme {
     if (Object.hasOwn(this.window, 'mathMedia')) {
       return Theme.Light;
     }
@@ -45,5 +53,16 @@ export class ThemeSyncDirective implements OnDestroy {
     ).matches;
 
     return isDark ? Theme.Dark : Theme.Light;
+  }
+
+  private setBrowserToolbarTheme(theme: Theme): void {
+    const themeColor: HTMLMetaElement | null = this.document.head.querySelector(
+      'meta[name=theme-color]'
+    );
+
+    const color: string =
+      theme === Theme.Light ? COLOR_NEUTRAL_1 : COLOR_NEUTRAL_1000;
+
+    this.renderer.setAttribute(themeColor, 'content', color);
   }
 }
