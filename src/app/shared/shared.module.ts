1a52e1167e22a16fc009d0597344879fa47980eb
vsezol <vsezold@gmail.com>
2023-03-01 10:38:13 +0300

    feature: remove useless things and improve performance

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,30 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, Type } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PupaCommonModule } from '@bimeister/pupakit.common';
-import { PupaFormsModule } from '@bimeister/pupakit.forms';
-import { IconDefinition, PupaIconsModule } from '@bimeister/pupakit.icons';
 import { PupaKitModule } from '@bimeister/pupakit.kit';
-import { PupaOverlaysModule } from '@bimeister/pupakit.overlays';
 import { NonNullablePipe } from './pipes/non-nullable.pipe';
 
 const MODULES: Type<unknown>[] = [
   CommonModule,
-  FormsModule,
-  ReactiveFormsModule,
   PupaCommonModule,
   PupaKitModule,
-  PupaFormsModule,
-  PupaOverlaysModule,
 ];
-
-const ICONS: IconDefinition[] = [];
 
 const PIPES: Type<unknown>[] = [NonNullablePipe];
 
 @NgModule({
-  declarations: [...PIPES],
-  imports: [...MODULES, PupaIconsModule.forFeature(ICONS)],
-  exports: [...MODULES, PupaIconsModule, ...PIPES],
+  declarations: [PIPES],
+  imports: [MODULES],
+  exports: [MODULES, PIPES],
 })
 export class SharedModule {}
